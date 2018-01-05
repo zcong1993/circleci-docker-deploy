196a3d1300408ba9c2bf20f45a0b4c2693a6a89b
zcong1993 <1005997919@qq.com>
2018-01-05 13:58:25 +0800

    tweak config

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -2,6 +2,7 @@
 
 export CI_BUILD_SHA="x${CIRCLE_SHA1:0:8}"
 
+# auto login
 docker() {
 	if [ ! -e ~/.docker/config.json ]; then
 		command docker login -u $DOCKERCLOUD_USER -p $DOCKERCLOUD_APIKEY
@@ -10,21 +11,15 @@ docker() {
 	command docker "$@"
 }
 
-# Automatically install docker-cloud
-docker_cloud() {
-	# if ! which docker-cloud >/dev/null 2>/dev/null; then
-	# 	# Install Docker Cloud
-	# 	sudo apt-get install python python-pip >/dev/null
-	# 	pip install docker-cloud >/dev/null
-	# fi
-
-	# command docker-cloud "$@"
+# push to docker cloud
+docker_push() {
 	echo Tag image...
 	docker tag $CIRCLE_PROJECT_REPONAME:$CI_BUILD_SHA $1/$2:$3
 	echo Push to docker cloud...
 	docker push $1/$2:$3
 }
 
+# build image
 docker_build() {
 	echo Build image...
 	docker build -t $CIRCLE_PROJECT_REPONAME:$CI_BUILD_SHA .
