838e4da13a9f1cbe2580bac4c5b3138e8c80770d
zcong1993 <1005997919@qq.com>
2018-01-05 13:28:51 +0800

    use docker push

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -3,18 +3,24 @@
 export CI_BUILD_SHA="x${CIRCLE_SHA1:0:8}"
 
 docker() {
+	if [ ! -e ~/.docker/config.json ]; then
+		command docker login -u $DOCKERCLOUD_USER -p $DOCKERCLOUD_APIKEY
+	fi
+
 	command docker "$@"
 }
 
 # Automatically install docker-cloud
 docker_cloud() {
-	if ! which docker-cloud >/dev/null 2>/dev/null; then
-		# Install Docker Cloud
-		sudo apt-get install python python-pip >/dev/null
-		pip install docker-cloud >/dev/null
-	fi
+	# if ! which docker-cloud >/dev/null 2>/dev/null; then
+	# 	# Install Docker Cloud
+	# 	sudo apt-get install python python-pip >/dev/null
+	# 	pip install docker-cloud >/dev/null
+	# fi
 
-	command docker-cloud "$@"
+	# command docker-cloud "$@"
+	echo Push to docker cloud
+	docker push $1/$2:$3
 }
 
 docker_tag_push() {
