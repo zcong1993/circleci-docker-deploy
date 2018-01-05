7d34e7b0cb103929fbc42c9ad358c8335bcb31e3
zcong1993 <1005997919@qq.com>
2018-01-05 12:46:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# circleci-test
+<!--
+[![Go Report Card](https://goreportcard.com/badge/github.com/zcong1993/circleci-test)](https://goreportcard.com/report/github.com/zcong1993/circleci-test)
+-->
+
+> my go project
+
+## License
+
+MIT &copy; zcong1993
---
+++ ci/prepare.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+export CI_BUILD_SHA="x${CIRCLE_SHA1:0:8}"
+
+docker() {
+	command docker "$@"
+}
+
+# Automatically install docker-cloud
+docker_cloud() {
+	if ! which docker-cloud >/dev/null 2>/dev/null; then
+		# Install Docker Cloud
+		apk --update add python py-pip >/dev/null
+		pip install docker-cloud >/dev/null
+	fi
+
+	command docker-cloud "$@"
+}
+
+docker_tag_push() {
+	echo Build image...
+
+	docker build -t $CIRCLE_PROJECT_REPONAME:$1 .
+
+	echo Tagging docker image...
+	docker tag $CIRCLE_PROJECT_REPONAME:$1 $2:$3
+
+	echo ${$CIRCLE_PROJECT_REPONAME} "$2$3"
+}
